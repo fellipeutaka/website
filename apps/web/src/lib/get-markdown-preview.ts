2446406a2897654f363d4884fab64cf46c96c637
Fellipe Utaka <fellipeutaka@gmail.com>
2024-04-25 14:37:20 -0300

    chore: bump deps

--- apps/web/src/lib/get-markdown-preview.ts
+++ apps/web/src/lib/get-markdown-preview.ts
@@ -1,6 +1,6 @@
-import { serialize } from "@utaka/mdx";
-
 export async function getMarkdownPreview(content: string) {
+  const { serialize } = await import("@utaka/mdx");
+
   return {
     result: await serialize(content),
   };
