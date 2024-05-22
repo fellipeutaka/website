23ea14b9afa38bc9f7083ec5f39604014fa5e873
Fellipe Utaka <fellipeutaka@gmail.com>
2024-05-22 15:43:07 -0300

    chore(db): bump deps

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -4,8 +4,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    url: env.DATABASE_URL,
   },
 } satisfies Config;
